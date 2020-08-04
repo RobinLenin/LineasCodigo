6f6be78df91a16e06cbd1fb8c37cae9ccb7acb7a
Robin Cordova <robincordova7@gmail.com>
2020-08-04 09:58:39 -0500

    Leer archivos de carpeta

--- src/lineascodigo1/LineasCodigo1.java
+++ src/lineascodigo1/LineasCodigo1.java
@@ -5,6 +5,8 @@
  */
 package lineascodigo1;
 
+import vista.VentanaPrincipal;
+
 /**
  *
  * @author robin
@@ -15,7 +17,8 @@ public class LineasCodigo1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        VentanaPrincipal ven = new VentanaPrincipal();
+        ven.show();
     }
     
 }
